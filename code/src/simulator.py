07478114253347ccd0b0772ffab8552f8e76eb1a
Jakob <31383697+Schabolon@users.noreply.github.com>
2023-12-18 17:58:46 +0100

    fix simulated output path

--- code/src/simulator.py
+++ code/src/simulator.py
@@ -2,20 +2,20 @@ import pickle
 import os
 from qiskit import QuantumCircuit
 from qiskit import transpile
-from qiskit_aer import AerSimulator
+from qiskit_aer import Aer
 from qiskit_aer.backends.qasm_simulator import AerBackend
 
 
-def walker(step):
+def walker(steps):
     circ = QuantumCircuit(4, 4)
-    for i in range(step):
-        if i % 3 == 0:
+    for step in range(steps):
+        if step % 3 == 0:
             circ.h(0)
             circ.h(1)
             circ.cx(0, 2)
 
             circ.barrier(0, 1, 2, 3)
-        elif i % 3 == 1:
+        elif step % 3 == 1:
             circ.cx(1, 3)
             circ.x(0)
 
@@ -30,7 +30,9 @@ def walker(step):
 
     return circ
 
-def save_quantum_circuit_simulation(circuits: list[QuantumCircuit], simulator: AerBackend, simulator_method: str, number_of_runs: int):
+
+def save_quantum_circuit_simulation(circuits: list[QuantumCircuit], simulator: AerBackend, simulator_method: str,
+                                    number_of_runs: int, circuit_name: str):
     print("Simulating circuit ...")
     print("Using {}.".format(simulator.__class__.__name__))
 
@@ -40,24 +42,21 @@ def save_quantum_circuit_simulation(circuits: list[QuantumCircuit], simulator: A
 
         result = simulator.run(circs_with_simulator, memory=True, shots=8000).result()
 
-        dict = result.to_dict()
-
-        basePath = "../data/simulated/"
+        base_path = os.path.join("../data/simulated", circuit_name)
         simulator_name = simulator.__class__.__name__.lower()
-        filename = os.path.join(basePath, "{}_{}-{:06d}.p".format(simulator_name, simulator_method, i))
-        pickle.dump(dict, open(filename, 'wb'))
+        filename = os.path.join(base_path, "{}_{}-{:06d}.p".format(simulator_name, simulator_method, i))
+        pickle.dump(result.to_dict(), open(filename, 'wb'))
 
     print("Finished simulating.")
 
 
 if __name__ == "__main__":
-    circs = []
+    circuits = []
     for step in range(9):
-        circ = walker(step)
-        circs.append(circ)
+        circuit = walker(step)
+        circuits.append(circuit)
 
     print("Using AerSimulator with Density Matrix.")
-    simulator = AerSimulator(method='density_matrix')
+    simulator = Aer.get_backend('aer_simulator_density_matrix')
 
-    save_quantum_circuit_simulation(circs, simulator, "density_matrix", 250)
-
+    save_quantum_circuit_simulation(circuits, simulator, "density_matrix", 250, "walker")
