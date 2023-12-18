66a5b354a4638e5aa40d1f7569c8156a2bf10649
Jakob <31383697+Schabolon@users.noreply.github.com>
2023-12-18 18:25:14 +0100

    extract data from quantum computer and simulated runs into mixed datasets

---
+++ code/src/extract_data.py
@@ -0,0 +1,34 @@
+import pickle
+import glob
+import os
+import tensorflow as tf
+
+if __name__ == "__main__":
+    data_pairs = []
+    # Add quantum computer runs
+    machine = "ibmq_athens"
+    for filename in sorted(glob.glob(os.path.join("../data/quantum_computer/walker", '{}-'.format(machine) + ('[0-9]' * 6) + '.p'))):
+        results = pickle.load(open(filename, 'rb'))
+        for t in range(len(results['results'])):
+            memory_int = list(map(lambda x: int(x, 16), results['results'][t]['data']['memory']))
+            data_pairs.append(("quantum_computer", memory_int))
+
+    # Add simulated runs
+    name = "aersimulator_density_matrix"
+    for filename in sorted(
+            glob.glob(os.path.join("../data/simulated/walker", '{}-'.format(name) + ('[0-9]' * 6) + '.p'))):
+        results = pickle.load(open(filename, 'rb'))
+        for t in range(len(results['results'])):
+            memory_int = list(map(lambda x: int(x, 16), results['results'][t]['data']['memory']))
+            data_pairs.append(("simulated", memory_int))
+
+    labels, features = zip(*data_pairs)
+    features = tf.constant(features, dtype=tf.int32) #TODO sollte ein float verwendet werden? (muss auf Werte zwischen 0 und 1 normiert werden?)
+    labels = tf.constant(labels, dtype=tf.string)
+
+    # Create dataset
+    dataset = tf.data.Dataset.from_tensor_slices((features, labels))
+
+    dataset = dataset.shuffle(buffer_size=len(features))
+
+    dataset.save("../data/mixed_datasets/walker_dataset")
