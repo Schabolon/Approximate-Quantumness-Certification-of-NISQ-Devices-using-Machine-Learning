ce968107d945d0600e689b5ffe2162edd4b5359d
Jakob <31383697+Schabolon@users.noreply.github.com>
2023-12-19 11:24:52 +0100

    add multiple machines + names for dataset generation

--- code/src/extract_data.py
+++ code/src/extract_data.py
@@ -6,21 +6,23 @@ import tensorflow as tf
 if __name__ == "__main__":
     data_pairs = []
     # Add quantum computer runs
-    machine = "ibmq_athens"
-    for filename in sorted(glob.glob(os.path.join("../data/quantum_computer/walker", '{}-'.format(machine) + ('[0-9]' * 6) + '.p'))):
-        results = pickle.load(open(filename, 'rb'))
-        for t in range(len(results['results'])):
-            memory_int = list(map(lambda x: int(x, 16), results['results'][t]['data']['memory']))
-            data_pairs.append(("quantum_computer", memory_int))
+    machines = ["ibmq_athens"]
+    for machine in machines:
+        for filename in sorted(glob.glob(os.path.join("../data/quantum_computer/walker", '{}-'.format(machine) + ('[0-9]' * 6) + '.p'))):
+            results = pickle.load(open(filename, 'rb'))
+            for t in range(len(results['results'])):
+                memory_int = list(map(lambda x: int(x, 16), results['results'][t]['data']['memory']))
+                data_pairs.append(("quantum_computer", memory_int))
 
     # Add simulated runs
-    name = "aersimulator_density_matrix"
-    for filename in sorted(
-            glob.glob(os.path.join("../data/simulated/walker", '{}-'.format(name) + ('[0-9]' * 6) + '.p'))):
-        results = pickle.load(open(filename, 'rb'))
-        for t in range(len(results['results'])):
-            memory_int = list(map(lambda x: int(x, 16), results['results'][t]['data']['memory']))
-            data_pairs.append(("simulated", memory_int))
+    names = ["aersimulator_density_matrix"]
+    for name in names:
+        for filename in sorted(
+                glob.glob(os.path.join("../data/simulated/walker", '{}-'.format(name) + ('[0-9]' * 6) + '.p'))):
+            results = pickle.load(open(filename, 'rb'))
+            for t in range(len(results['results'])):
+                memory_int = list(map(lambda x: int(x, 16), results['results'][t]['data']['memory']))
+                data_pairs.append(("simulated", memory_int))
 
     labels, features = zip(*data_pairs)
     features = tf.constant(features, dtype=tf.int32) #TODO sollte ein float verwendet werden? (muss auf Werte zwischen 0 und 1 normiert werden?)
