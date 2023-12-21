876dc6ed28f81231a96f49e7026f7361b2cf5460
Jakob <31383697+Schabolon@users.noreply.github.com>
2023-12-21 17:43:25 +0100

    update save_circuits_to_image

--- code/src/quantum_circuits/util.py
+++ code/src/quantum_circuits/util.py
@@ -3,13 +3,18 @@ Provides utility functions for quantum circuits.
 """
 from matplotlib import pyplot as plt
 from qiskit import QuantumCircuit
-from quantum_circuits import walker
+from quantum_circuits import walker, ramsey, walkerSimple, walkerSingleMeasures
 
 
-def save_circuit_to_image(circ: QuantumCircuit, filename):
-    circ.draw(output='mpl')
-    plt.savefig(filename)
+def save_circuits_to_image(circs: list[QuantumCircuit], circuit_name: str):
+    for i in range(len(circs)):
+        circs[i].draw(output='mpl')
+        plt.savefig("../../circuit_images/{}-step-{}.svg".format(circuit_name, i + 1))
+        plt.close()
 
 
 if __name__ == "__main__":
-    print(walker.__walker_circuit(9))
+    save_circuits_to_image(walker.get_default_circuits(), "walker")
+    save_circuits_to_image(walkerSimple.get_default_circuits(), "walker-simple")
+    save_circuits_to_image(walkerSingleMeasures.get_default_circuits(), "walker-single-measures")
+    save_circuits_to_image(ramsey.get_default_circuits(), "ramsey")
