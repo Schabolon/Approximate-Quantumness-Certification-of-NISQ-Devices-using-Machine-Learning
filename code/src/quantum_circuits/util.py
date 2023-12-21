968fc632fc92430b4c87394476d0226d23bed97e
Jakob <31383697+Schabolon@users.noreply.github.com>
2023-12-21 18:20:54 +0100

    add circuit as text

--- code/src/quantum_circuits/util.py
+++ code/src/quantum_circuits/util.py
@@ -13,8 +13,23 @@ def save_circuits_to_image(circs: list[QuantumCircuit], circuit_name: str):
         plt.close()
 
 
-if __name__ == "__main__":
+def generate_images_for_all_circuits():
     save_circuits_to_image(walker.get_default_circuits(), "walker")
     save_circuits_to_image(walkerSimple.get_default_circuits(), "walker-simple")
     save_circuits_to_image(walkerSingleMeasures.get_default_circuits(), "walker-single-measures")
     save_circuits_to_image(ramsey.get_default_circuits(), "ramsey")
+
+
+if __name__ == "__main__":
+    # Display circuits as text
+    print("Walker Circuit:")
+    #print(walker.get_default_circuits()[-1])
+    print("")
+    print("Walker Simple Circuit:")
+    print(walkerSimple.get_default_circuits()[-1])
+    print("")
+    print("Walker Single Measures Circuit:")
+    print(walkerSingleMeasures.get_default_circuits()[-1])
+    print("")
+    print("Ramsey Circuit:")
+    print(ramsey.get_default_circuits()[-1])
